3ef3fb93a730411c68bcc82475fa87ab07eb1578
Simon Goodman <simon.goodman@just-eat.com>
2019-12-02 10:35:44 +0000

    Add Day 2 Part 1

---
+++ AdventOfCode2019/Day2/Intcode.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019.Day2
+{
+    class Intcode
+    {
+        public static Intcode LoadFromFile(string filename)
+        {
+            var memory = File.ReadAllLines(filename)
+                .First()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            return new Intcode(memory);
+        }
+
+
+        private int[] _memory;
+        private int _programCounter = 0;
+        private Dictionary<int, Action> _operations;
+
+        private Intcode(int[] memory)
+        {
+            _memory = memory;
+
+            _operations = new Dictionary<int, Action>
+            {
+                {1, Add },
+                {2, Multiply },
+            };
+        }
+
+        public Intcode Repair(int memoryLocation, int newValue)
+        {
+            _memory[memoryLocation] = newValue;
+            return this;
+        }
+
+        public int ReadMemory(int memoryLocation)
+        {
+            return _memory[memoryLocation];
+        }
+
+        public Intcode Execute()
+        {
+            while (true)
+            {
+                var opCode = _memory[_programCounter];
+                if (opCode == 99)
+                {
+                    break;
+                }
+
+                _operations[opCode]();
+            }
+
+            return this;
+        }
+
+        private void Add()
+        {
+            var aPointer = _memory[_programCounter + 1];
+            var bPointer = _memory[_programCounter + 2];
+            var cPointer = _memory[_programCounter + 3];
+
+            _memory[cPointer] = _memory[aPointer] + _memory[bPointer];
+
+            _programCounter += 4;
+        }
+
+        private void Multiply()
+        {
+            var aPointer = _memory[_programCounter + 1];
+            var bPointer = _memory[_programCounter + 2];
+            var cPointer = _memory[_programCounter + 3];
+
+            _memory[cPointer] = _memory[aPointer] * _memory[bPointer];
+
+            _programCounter += 4;
+        }
+    }
+}
--- AdventOfCode2019/Program.cs
+++ AdventOfCode2019/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using AdventOfCode2019.Day1;
+using AdventOfCode2019.Day2;
 
 namespace AdventOfCode2019
 {
@@ -12,6 +13,9 @@ namespace AdventOfCode2019
 
             var totalFuelRequirements = FuelRequirements.LoadFromFile("Day1/ModuleMasses.txt").GetFuelRequirementsWithFuelForFuel();
             Console.WriteLine($"Day 1 Part 2: {totalFuelRequirements}");
+
+            var day2Part1Result = Intcode.LoadFromFile("Day2/GravityAssistProgram.txt").Repair(1, 12).Repair(2, 2).Execute().ReadMemory(0);
+            Console.WriteLine($"Day 2 Part 1: {day2Part1Result}");
 
             Console.ReadKey();
         }
